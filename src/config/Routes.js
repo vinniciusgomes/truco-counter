4456b1f6172133808a8d4198176cdc452f8848d2
vinniciusgomes <vinniciusgomes@outlook.com.br>
2019-05-18 14:03:07 -0300

    Criando projeto com template Vife

---
+++ src/config/Routes.js
@@ -0,0 +1,17 @@
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
+
+
+import MainScreen from "~/screens/Main";
+
+const AppNavigator = createSwitchNavigator(
+  {
+    Main: {
+      screen: MainScreen,
+    }
+  },
+  {
+    initialRouteName: "Main"
+  }
+);
+
+export default createAppContainer(AppNavigator);

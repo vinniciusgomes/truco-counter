6b154d4917ef45c4c20ec7803d8d7eb63c2d4e05
Vinnicius Gomes <vinniciusgomes@outlook.com.br>
2019-05-31 12:03:10 -0300

    Adicionando future Truco, melhorando StatusBar na Home

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -4,12 +4,11 @@ import {
   View,
   StatusBar,
   TouchableOpacity,
-  StyleSheet,
   Alert,
-  Image,
-  Dimensions,
-  Platform
+  Image
 } from "react-native";
+
+import { styles } from "./styles";
 
 /**
  *
@@ -21,20 +20,25 @@ class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      scoreEles: 0,
-      scoreNos: 0,
-      partidasNos: 0,
-      partidasEles: 0
+      scoreTime2: 0,
+      scoreTime1: 0,
+      partidasTime1: 0,
+      partidasTime2: 0,
+      darkMode: true,
+      truco: false
     };
   }
   static navigationOptions = {
     header: null
   };
 
-  addScoreNos = () => {
-    if (this.state.scoreNos >= 12) {
+  addScoreTime1 = () => {
+    if (this.state.scoreTime1 >= 11) {
+      if (this.state.scoreTime1 < 12) {
+        this.setState({ scoreTime1: this.state.scoreTime1 + 1 });
+      }
       Alert.alert(
-        "Nós ganhamos!",
+        "O Time 1 venceu!",
         "Deseja continuar a partida?",
         [
           {
@@ -46,31 +50,44 @@ class Home extends Component {
             text: "Sim",
             onPress: () =>
               this.setState({
-                scoreNos: 0,
-                scoreEles: 0,
-                partidasNos: this.state.partidasNos + 1
+                scoreTime1: 0,
+                scoreTime2: 0,
+                partidasTime1: this.state.partidasTime1 + 1
               })
           }
         ],
         { cancelable: false }
       );
     } else {
-      this.setState({
-        scoreNos: this.state.scoreNos + 1
-      });
+      if (this.state.truco === true) {
+        this.setState({
+          scoreTime1: this.state.scoreTime1 + 3
+        });
+      } else {
+        this.setState({
+          scoreTime1: this.state.scoreTime1 + 1
+        });
+      }
     }
   };
 
-  removeScoreNos = () => {
-    if (this.state.scoreNos >= 1) {
-      this.setState({ scoreNos: this.state.scoreNos - 1 });
+  removeScoreTime1 = () => {
+    if (this.state.scoreTime1 >= 1) {
+      if (this.state.truco === true) {
+        this.setState({ scoreTime1: this.state.scoreTime1 - 3 });
+      } else {
+        this.setState({ scoreTime1: this.state.scoreTime1 - 1 });
+      }
     }
   };
 
-  addScoreEles = () => {
-    if (this.state.scoreEles >= 12) {
+  addScoreTime2 = () => {
+    if (this.state.scoreTime2 >= 11) {
+      if (this.state.scoreTime2 < 12) {
+        this.setState({ scoreTime2: this.state.scoreTime2 + 1 });
+      }
       Alert.alert(
-        "Eles vencerão!",
+        "O Time 2 venceu!",
         "Deseja continuar a partida?",
         [
           {
@@ -82,186 +99,154 @@ class Home extends Component {
             text: "Sim",
             onPress: () =>
               this.setState({
-                scoreEles: 0,
-                scoreNos: 0,
-                partidasEles: this.state.partidasEles + 1
+                scoreTime2: 0,
+                scoreTime1: 0,
+                partidasTime2: this.state.partidasTime2 + 1
               })
           }
         ],
         { cancelable: false }
       );
     } else {
-      this.setState({
-        scoreEles: this.state.scoreEles + 1
-      });
+      if (this.state.truco === true) {
+        this.setState({
+          scoreTime2: this.state.scoreTime2 + 3
+        });
+      } else {
+        this.setState({
+          scoreTime2: this.state.scoreTime2 + 1
+        });
+      }
     }
   };
 
-  removeScoreEles = () => {
-    if (this.state.scoreEles >= 1) {
-      this.setState({ scoreEles: this.state.scoreEles - 1 });
+  removeScoreTime2 = () => {
+    if (this.state.scoreTime2 >= 1) {
+      if (this.state.truco === true) {
+        this.setState({ scoreTime2: this.state.scoreTime2 - 3 });
+      } else {
+        this.setState({ scoreTime2: this.state.scoreTime2 - 1 });
+      }
     }
   };
 
   resetScore = () => {
-    this.setState({
-      scoreNos: 0,
-      scoreEles: 0,
-      partidasNos: 0,
-      partidasEles: 0
-    });
+    Alert.alert(
+      "Cuidado",
+      "Você deseja reiniciar a partida?",
+      [
+        {
+          text: "Não"
+        },
+        {
+          text: "Sim",
+          style: "destructive",
+          onPress: () =>
+            this.setState({
+              scoreTime1: 0,
+              scoreTime2: 0,
+              partidasTime1: 0,
+              partidasTime2: 0
+            })
+        }
+      ],
+      { cancelable: false }
+    );
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <StatusBar barStyle="light-content" backgroundColor="#000" />
-        <Image
-          source={require("~/assets/img/bg.jpg")}
-          style={{ height: 200, width: "auto" }}
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor={"transparent"}
+          translucent
         />
-        <View style={{ alignItems: "center", marginTop: -80 }}>
+        <Image source={require("~/assets/img/bg.jpg")} style={styles.bg} />
+        <View style={styles.scoreCard}>
           <View style={styles.scoreboard}>
             <Image
               source={require("~/assets/img/cards.png")}
-              style={{ width: 30, height: 30 }}
+              style={styles.iconCard}
             />
-            <View
-              style={{
-                flexDirection: "row",
-                alignItems: "center",
-                justifyContent: "center"
-              }}
-            >
-              <Text style={{ fontSize: 50, fontWeight: "500", color: "#fff" }}>
-                {this.state.scoreNos}
-              </Text>
-              <Text
-                style={{
-                  fontSize: 20,
-                  fontWeight: "100",
-                  margin: 20,
-                  color: "#909090"
-                }}
-              >
-                x
-              </Text>
-              <Text style={{ fontSize: 50, fontWeight: "500", color: "#fff" }}>
-                {this.state.scoreEles}
-              </Text>
+            <View style={styles.pontuacao}>
+              <TouchableOpacity onPress={() => this.addScoreTime1()}>
+                <Text style={styles.scoreText}>{this.state.scoreTime1}</Text>
+              </TouchableOpacity>
+              <Text style={styles.x}>x</Text>
+              <TouchableOpacity onPress={() => this.addScoreTime2()}>
+                <Text style={styles.scoreText}>{this.state.scoreTime2}</Text>
+              </TouchableOpacity>
             </View>
             <Text style={{ color: "#909090" }}>
-              {this.state.partidasNos} x {this.state.partidasEles}
+              {this.state.partidasTime1} x {this.state.partidasTime2}
             </Text>
           </View>
         </View>
         <View style={styles.contentContainer}>
-          <View
-            style={{
-              flexDirection: "row",
-              justifyContent: "space-between",
-              paddingBottom: 20
-            }}
-          >
-            <Text style={{ color: "#fff", fontWeight: "500" }}>Nós</Text>
-            <Text style={{ color: "#fff", fontWeight: "500" }}>Eles</Text>
+          <View style={styles.timeSeparator}>
+            <Text style={styles.text500}>Time 1</Text>
+            <Text style={styles.text500}>Time 2</Text>
           </View>
           <View
             style={{ flexDirection: "row", justifyContent: "space-between" }}
           >
             <View style={{ flexDirection: "row", justifyContent: "center" }}>
-              <TouchableOpacity onPress={() => this.addScoreNos()}>
-                <View
-                  style={{
-                    width: 50,
-                    height: 50,
-                    borderRadius: 25,
-                    backgroundColor: "#1b1b1b",
-                    justifyContent: "center",
-                    alignItems: "center"
-                  }}
-                >
-                  <Text style={{ color: "#fff", fontSize: 20 }}>+1</Text>
+              <TouchableOpacity onPress={() => this.addScoreTime1()}>
+                <View style={styles.addButton}>
+                  <Text style={{ color: "#fff", fontSize: 20 }}>
+                    +{this.state.truco ? "3" : "1"}
+                  </Text>
                 </View>
               </TouchableOpacity>
               <TouchableOpacity
-                disabled={this.state.scoreNos == 0 ? true : false}
-                onPress={() => this.removeScoreNos()}
-                style={{ opacity: this.state.scoreNos == 0 ? 0.5 : 1 }}
+                disabled={this.state.scoreTime1 == 0 ? true : false}
+                onPress={() => this.removeScoreTime1()}
+                style={{ opacity: this.state.scoreTime1 == 0 ? 0.5 : 1 }}
               >
-                <View
-                  style={{
-                    width: 50,
-                    height: 50,
-                    borderRadius: 25,
-                    backgroundColor: "#1b1b1b",
-                    marginLeft: 20,
-                    justifyContent: "center",
-                    alignItems: "center"
-                  }}
-                >
-                  <Text style={{ color: "#fff", fontSize: 20 }}>-1</Text>
+                <View style={styles.menos}>
+                  <Text style={{ color: "#fff", fontSize: 20 }}>
+                    -{this.state.truco ? "3" : "1"}
+                  </Text>
                 </View>
               </TouchableOpacity>
             </View>
-            <View
-              style={{
-                width: 2,
-                height: 150,
-                marginTop: -50,
-                backgroundColor: "#1b1b1b"
-              }}
-            />
+            <View style={styles.separator} />
             <View style={{ flexDirection: "row", justifyContent: "center" }}>
-              <TouchableOpacity onPress={() => this.addScoreEles()}>
-                <View
-                  style={{
-                    width: 50,
-                    height: 50,
-                    borderRadius: 25,
-                    backgroundColor: "#1b1b1b",
-                    justifyContent: "center",
-                    alignItems: "center"
-                  }}
-                >
-                  <Text style={{ color: "#fff", fontSize: 20 }}>+1</Text>
+              <TouchableOpacity onPress={() => this.addScoreTime2()}>
+                <View style={styles.addButton}>
+                  <Text style={{ color: "#fff", fontSize: 20 }}>
+                    +{this.state.truco ? "3" : "1"}
+                  </Text>
                 </View>
               </TouchableOpacity>
               <TouchableOpacity
-                disabled={this.state.scoreEles == 0 ? true : false}
-                onPress={() => this.removeScoreEles()}
-                style={{ opacity: this.state.scoreEles == 0 ? 0.5 : 1 }}
+                disabled={this.state.scoreTime2 == 0 ? true : false}
+                onPress={() => this.removeScoreTime2()}
+                style={{ opacity: this.state.scoreTime2 == 0 ? 0.5 : 1 }}
               >
-                <View
-                  style={{
-                    width: 50,
-                    height: 50,
-                    borderRadius: 25,
-                    backgroundColor: "#1b1b1b",
-                    marginLeft: 20,
-                    justifyContent: "center",
-                    alignItems: "center"
-                  }}
-                >
-                  <Text style={{ color: "#fff", fontSize: 20 }}>-1</Text>
+                <View style={styles.menos}>
+                  <Text style={{ color: "#fff", fontSize: 20 }}>
+                    -{this.state.truco ? "3" : "1"}
+                  </Text>
                 </View>
               </TouchableOpacity>
             </View>
           </View>
           <View style={{ alignItems: "center" }}>
+            <TouchableOpacity
+              onPress={() => this.setState({ truco: !this.state.truco })}
+            >
+              <View style={styles.buttonOutline}>
+                <Text style={{ color: "rgb(129,129,129)" }}>
+                  {this.state.truco ? "DESABILITAR TRUCO" : "TRUCO"}
+                </Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View style={{ alignItems: "center" }}>
             <TouchableOpacity onPress={() => this.resetScore()}>
-              <View
-                style={{
-                  width: Dimensions.get("window").width - 200,
-                  height: 40,
-                  marginTop: 50,
-                  borderRadius: 1,
-                  borderWidth: 1,
-                  borderColor: "rgb(129,129,129)",
-                  justifyContent: "center",
-                  alignItems: "center"
-                }}
-              >
+              <View style={{ marginTop: 30 }}>
                 <Text style={{ color: "rgb(129,129,129)" }}>ZERAR PLACAR</Text>
               </View>
             </TouchableOpacity>
@@ -271,44 +256,5 @@ class Home extends Component {
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#121212"
-  },
-  header: {
-    borderBottomColor: "#fff",
-    backgroundColor: "#fff"
-  },
-  contentContainer: {
-    paddingHorizontal: 20,
-    paddingTop: 40
-  },
-  scoreboard: {
-    width: Dimensions.get("window").width - 40,
-    height: 160,
-    backgroundColor: "#1b1b1b",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 12
-    },
-    shadowOpacity: 0.58,
-    shadowRadius: 16.0,
-    elevation: 24,
-    borderRadius: 2,
-    justifyContent: "center",
-    alignItems: "center"
-  },
-  resetButton: {
-    marginTop: 50,
-    borderRadius: 1,
-    borderWidth: 0.5,
-    borderColor: "#fff",
-    width: Dimensions.get("window").width - 40,
-    height: 50
-  }
-});
 
 export default Home;
---
+++ src/screens/Home/styles.js
@@ -0,0 +1,110 @@
+import { StyleSheet, Dimensions } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121212"
+  },
+  header: {
+    borderBottomColor: "#fff",
+    backgroundColor: "#fff"
+  },
+  bg: {
+    height: 200,
+    width: "auto"
+  },
+  scoreCard: {
+    alignItems: "center",
+    marginTop: -80
+  },
+  contentContainer: {
+    paddingHorizontal: 20,
+    paddingTop: 40
+  },
+  scoreboard: {
+    width: Dimensions.get("window").width - 40,
+    height: 160,
+    backgroundColor: "#1b1b1b",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 12
+    },
+    shadowOpacity: 0.58,
+    shadowRadius: 16.0,
+    elevation: 24,
+    borderRadius: 2,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  resetButton: {
+    marginTop: 50,
+    borderRadius: 1,
+    borderWidth: 0.5,
+    borderColor: "#fff",
+    width: Dimensions.get("window").width - 40,
+    height: 50
+  },
+  iconCard: {
+    width: 30,
+    height: 30
+  },
+  pontuacao: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  x: {
+    fontSize: 20,
+    fontWeight: "100",
+    margin: 20,
+    color: "#909090"
+  },
+  scoreText: {
+    fontSize: 50,
+    fontWeight: "500",
+    color: "#fff"
+  },
+  timeSeparator: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingBottom: 20
+  },
+  text500: {
+    color: "#fff",
+    fontWeight: "500"
+  },
+  addButton: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "#1b1b1b",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  menos: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: "#1b1b1b",
+    marginLeft: 20,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  buttonOutline: {
+    width: Dimensions.get("window").width - 200,
+    height: 40,
+    marginTop: 50,
+    borderRadius: 1,
+    borderWidth: 1,
+    borderColor: "rgb(129,129,129)",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  separator: {
+    width: 2,
+    height: 150,
+    marginTop: -50,
+    backgroundColor: "#1b1b1b"
+  },
+});
